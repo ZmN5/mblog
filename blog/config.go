03689f01e040e94519b7e1acf25403a6c9246c78
fucangyu <fucangyu@ein.plus>
2018-09-16 19:30:23 +0800

    change storage path

--- blog/config.go
+++ blog/config.go
@@ -1,6 +1,6 @@
 package blog
 
-const MARKDOWN_PATH = "/data/markdown"
-const HTML_PATH = "/data/html"
+const MARKDOWN_PATH = "/data/blog/markdown"
+const HTML_PATH = "/data/blog/html"
 
 const AUTH = ""
